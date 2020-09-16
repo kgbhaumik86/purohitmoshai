b5b6456c8141e1530247f3f720558d4de7eeae62
kgbhaumik86 <kgbhaumik86@gmail.com>
2020-09-16 19:34:49 +0530

    Updates

---
+++ precache-manifest.3ea5b4c0b0dfbfdfa64a8c0e90f2b1a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a7ad723b1f34d5afa77c568b0fb6f5",
+    "url": "/purohitmoshai/index.html"
+  },
+  {
+    "revision": "00f99f54d07652f7f9b3",
+    "url": "/purohitmoshai/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e52ca2bc000a92ac1377",
+    "url": "/purohitmoshai/static/js/2.d0fe23f5.chunk.js"
+  },
+  {
+    "revision": "76dd867e6fd5d7480b160d20fdfc695f",
+    "url": "/purohitmoshai/static/js/2.d0fe23f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f99f54d07652f7f9b3",
+    "url": "/purohitmoshai/static/js/main.737195f0.chunk.js"
+  },
+  {
+    "revision": "a832107c7add6315fcb7",
+    "url": "/purohitmoshai/static/js/runtime-main.f43c64bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/purohitmoshai/static/media/logo.5d5d9eef.svg"
+  }
+]);
